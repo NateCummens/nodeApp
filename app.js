4b5882deb3bf1f4c125900725c6029b643970a8c
NateCummens <78138781+NateCummens@users.noreply.github.com>
2024-02-10 17:17:00 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(
+    8080, 
+    () => console.log(`app is running on http://localhost:${8080}`)
+)
+
+app.get('/tasks', (req, res, next) =>{
+    if(req.query.filter == 'bydate'){
+        console.log('here')
+    }
+
+    res.send({response:'Hello World'})
+})
